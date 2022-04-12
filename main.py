f4600a1b0a5e703f26bc1fbc4a7b8558f803d2dd
Louis Roblin <louis@MacBook-Air-de-Louis.local>
2022-04-12 09:14:17 +0200

    Add helpers.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from bottle import route, run
+from helpers import hello
 import sys
 
 
 @route("/")
 def index():
-    return "<b>Hello World !</b>"
+    return hello()
 
 
 run(host="0.0.0.0", port=sys.argv[1], reloader=True)
